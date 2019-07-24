601787eaf21f055ef315bd8703af7ffbd727a6e3
anajodai <anajbrancos@gmail.com>
2019-07-24 12:06:29 -0300

    sql django

---
+++ models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Pessoa(models.Model):
+
+    nome = models.CharField(max_length=255)
+    idade = models.IntegerField()
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
